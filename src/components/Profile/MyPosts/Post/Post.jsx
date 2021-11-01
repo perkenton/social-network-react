648d3b075807e15b9bc4577d0fa74af3b181593d
Alexander Vasiliev <selado@ya.ru>
2021-11-02 02:35:46 +0500

    Исправлены имена классов компонента Post

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,9 +5,9 @@ import userPhoto from '../../../../images/user-photo.png';
 const Post = (props) => {
   return (
     <div className={ styles.post }>
-      <img src={ userPhoto } alt="Фото автора" className={ styles.post__authorPhoto} />
-      <p className={ styles.post__text }>{ props.text }</p>
-      <p className={ styles.post__likes }>Likes: { props.likesCount }</p>
+      <img src={ userPhoto } alt="Avatar" className={ styles.authorPhoto} />
+      <p className={ styles.text }>{ props.text }</p>
+      <p className={ styles.likes }>Likes: { props.likesCount }</p>
     </div>
   );
 }
