10a6ff9a9508426d55b9da506a8977273f494f5b
Alexander Vasiliev <selado@ya.ru>
2021-11-02 02:32:02 +0500

    Исправлены имена классов компонента NewPost

--- src/components/Profile/MyPosts/NewPost/NewPost.jsx
+++ src/components/Profile/MyPosts/NewPost/NewPost.jsx
@@ -4,8 +4,8 @@ import styles from './NewPost.module.scss';
 const NewPost = () => {
   return (
     <div className={ styles.newPost }>
-      <textarea name="newPost" id="newPost" cols="30" rows="5" className={ styles.newPost__textarea } />
-      <button className={ styles.newPost__addButton }>Добавить</button>
+      <textarea name="newPost" id="newPost" cols="30" rows="5" className={ styles.textarea } />
+      <button className={ styles.addButton }>Добавить</button>
     </div>
   );
 }
