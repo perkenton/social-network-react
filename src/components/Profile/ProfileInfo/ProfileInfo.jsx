a20a512597a2e69bc590b2da90be43f20fe5ef33
Alexander Vasiliev <selado@ya.ru>
2021-11-02 01:16:54 +0500

    Исправлен компонент с данными пользователя и его стили

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -6,19 +6,29 @@ import styles from './ProfileInfo.module.scss';
 const ProfileInfo = () => {
   return (
     <>
-      <div className={ styles.profile__topImageWrapper }>
-        <img src={ photo } alt="Top photo" className={ styles.profile__topImage } />
+      <div className={ styles.topImageWrapper }>
+        <img src={ photo } alt="Top photo" className={ styles.topImage } />
       </div>
 
-      <div className={ styles.profile__user}>
-        <img src={ userPhoto } alt="User photo" className={ styles.profile__userPhoto } />
-        <div className={ styles.profile__userInfo }>
-          <p className={ styles.profile__userName }>Aleksanchello</p>
-          <p className={ styles.profile__userBirthday }>Date of Birth: 15.05.1990</p>
-          <p className={ styles.profile__userCity }>City: City</p>
-          <p className={ styles.profile__userEducation }>Education: Ya.Praktikum</p>
-          <p className={ styles.profile__userSite }>
-            Website: <a href="https://github.com/perkenton" className={ styles.profile__userSiteLink }>www.github.com/perkenton</a>
+      <div className={ styles.userInfoBlock}>
+        <img src={ userPhoto } alt="User photo" className={ styles.userPhoto } />
+        <div>
+          <p className={ styles.userName }>Aleksanchello</p>
+          <p className={ styles.userInfoItem }>
+            <span className={ styles.secondaryText }>Дата рождения </span>
+            <span className={ styles.primaryText }>01.01.1970</span>
+          </p>
+          <p className={ styles.userInfoItem }>
+            <span className={ styles.secondaryText }>Город </span>
+            <span className={ styles.primaryText }>Гдетоград</span>
+          </p>
+          <p className={ styles.userInfoItem }>
+            <span className={ styles.secondaryText }>Образование </span>
+            <span className={ styles.primaryText }>Ya.Praktikum</span>
+          </p>
+          <p className={ styles.userInfoItem }>
+            <span className={ styles.secondaryText }>Сайт </span>
+            <a href="https://github.com/perkenton" className={ styles.siteLink }>www.github.com/perkenton</a>
           </p>
         </div>
       </div>
